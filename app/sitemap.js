407a0dc0238702430fa2866c5da495bc158cac2b
Abdul Basit Ali <aliabdulbasit2002@gmail.com>
2024-11-18 11:42:18 +0000

    fixed sitemap.xml

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,28 +1,52 @@
 export default function sitemap() {
   return [
     {
-      url: "https://www.codetrainafrica.com/about-us",
+      url: "https://codetrainafrica.com/about-us",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+      changeFrequency: "daily",
+      priority: 0.7,
     },
     {
-      url: "https://www.codetrainafrica.com/courses/software-engineering",
+      url: "https://codetrainafrica.com/courses/software-engineering",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+      changeFrequency: "daily",
+      priority: 0.7,
     },
     {
-      url: "https://www.codetrainafrica.com/courses/ui-ux-design",
+      url: "https://codetrainafrica.com/courses/ui-ux-design",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+      changeFrequency: "daily",
+      priority: 0.7,
     },
     {
       url: "https://codetrainafrica.com/courses/codetrain-solara",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+    {
+      url: "https://codetrainafrica.com/success-stories",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+    {
+      url: "https://codetrainafrica.com/hire-a-graduate",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+    {
+      url: "https://codetrainafrica.com/partnerships",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+    {
+      url: "https://codetrainafrica.com/donate",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
     },
   ];
 }

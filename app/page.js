037643715e06a6810e335d4b447c1338223610f0
Harrison <odameharrison13@gmail.com>
2025-10-15 11:12:12 +0000

    Changed the hero section text to Admission is still in progress, join the next Cohort!

--- app/page.js
+++ app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
                 </Link>
               </div>
               <p className="mt-8 text-xs font-extrabold  md:text-base text-textColor">
-                * Admission is in progress, join the September 2025 Cohort!
+                * Admission is still in progress, join the next Cohort!
               </p>
             </div>
           </div>

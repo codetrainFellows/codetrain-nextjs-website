2ffcd744fdef74700666b5685b616eebdd1edcff
JOSH-MOTION <joshuadoe168@gmail.com>
2026-02-24 14:37:39 +0000

    done

--- app/page.js
+++ app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
                 </Link>
               </div>
               <p className="mt-8 text-xs font-extrabold  md:text-base text-textColor">
-                * Admission is still in progress, join the January Cohort!
+                * Admission is still in progress, join May Cohort!
               </p>
             </div>
           </div>
